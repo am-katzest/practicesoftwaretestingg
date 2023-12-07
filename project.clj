1cb76345838c7d28fc811dfaaf4bf6d221c7ff14
bestestest kitteh <236551@edu.p.lodz.pl>
2023-12-07 21:34:50 +0100

    add cucumber thing dependency

--- project.clj
+++ project.clj
@@ -4,5 +4,6 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.11.1"]
-                 [etaoin "1.0.40"]]
+                 [etaoin "1.0.40"]
+                 [new-ways/clj-cukes "0.2.0"]]
   :repl-options {:init-ns testing-gui.core})
